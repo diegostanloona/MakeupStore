031b794e425bb35f66c3c863af39e1b25d2883f1
Diego Gomez Rodriguez <diegostanloona@icloud.com>
2021-11-03 13:44:29 -0500

    Defined file structure for pages and components

--- makeup-store/src/App.js
+++ makeup-store/src/App.js
@@ -5,3 +5,13 @@ function App() {
 }
 
 export default App;
+
+/**
+ * https://makeup-api.herokuapp.com/api/v1/products.json
+ * 1. Landing page con formulario de contacto (validaciones)
+ * 2. Lista de items en la tienda (filtros por marca, categoría, tag y keyword) (ordenar por precio, fecha)
+ * 3. WishList
+ * 4. Carrito con formulario (validaciones)
+ * 5. Registro e inicio de sesión (demo user)
+ * 6. Product page
+ */
